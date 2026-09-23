72063b05fa1b817f020c4ea046765f7cd81ad05e
Sean Murphy <sean@iamseanmurphy.com>
2026-09-23 16:53:48 +0000

    1.50.0

--- accessibility-checker/vendor/composer/installed.php
+++ accessibility-checker/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'equalizedigital/accessibility-checker',
-        'pretty_version' => 'v1.49.0',
-        'version' => '1.49.0.0',
-        'reference' => '77a4f7342cf4539076a439866033bbcf81c86c6e',
+        'pretty_version' => 'v1.50.0',
+        'version' => '1.50.0.0',
+        'reference' => '77c18af7face79be2d5edb51b68d18b6d2711e30',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -31,9 +31,9 @@
             'dev_requirement' => false,
         ),
         'equalizedigital/accessibility-checker' => array(
-            'pretty_version' => 'v1.49.0',
-            'version' => '1.49.0.0',
-            'reference' => '77a4f7342cf4539076a439866033bbcf81c86c6e',
+            'pretty_version' => 'v1.50.0',
+            'version' => '1.50.0.0',
+            'reference' => '77c18af7face79be2d5edb51b68d18b6d2711e30',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
